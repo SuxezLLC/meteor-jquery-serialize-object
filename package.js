f37faecf4cb17f96146df3698ec1914205e2e9e8
Roman Geber <rgeber@suxez.com>
2015-04-19 00:10:30 +0200

    Added github repo URL

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
     name: 'suxez:jquery-serialize-object',
     version: '1.0.0',
     // Brief, one-line summary of the package.
-    summary: 'Provides Ben Alman\'s jquery serializeObject method.' ,
+    summary: 'Provides Ben Alman\'s jQuery serializeObject method.' ,
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/SuxezLLC/meteor-jquery-serialize-object',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
